8a6bd6a34e79ab8aa1c376917b07dad49c2d593b
Douglas <douglascbs.contact@gmail.com>
2024-03-26 10:30:50 -0300

    adicionando uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imageMin = require('gulp-imagemin')
+const imageMin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function compressSass () {
     return gulp.src('./src/styles/*.scss')
@@ -14,8 +15,15 @@ function compressImage () {
         .pipe(gulp.dest('./dist/images'))
 }
 
-exports.default = gulp.parallel(compressSass, compressImage);
+function compressJs () {
+    return gulp.src('./src/scripts/*.js') 
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts'))
+}
+
+exports.default = gulp.parallel(compressSass, compressImage, compressJs);
 
 exports.watch = () =>  {
     gulp.watch('./src/styles/*.scss', gulp.parallel(compressSass))
+    gulp.watch('./src/script/*.js', gulp.parallel(compressJs))
 }
